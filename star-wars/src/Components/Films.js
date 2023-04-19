69dbfec1fef38160ff76027d787ed431b4394604
ekeukeni <ukenieke@gmail.com>
2023-04-19 21:01:45 +0100

    star-wars-app

---
+++ star-wars/src/Components/Films.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+//import axios from 'axios';
+import MoreInfoBtn from './MoreInfo';
+
+
+
+const Films = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+
+    fetch(`https://swapi.dev/api/films`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`This is an HTTP Error: The status is ${response.status}`)
+                }
+                return response.json()
+            })
+            .then((actualData) => {
+                setData(actualData.results)
+                setError(null)
+            })
+            .catch((error) => {
+                console.log(error)
+                setError(error)
+                setData(null)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+
+
+    // const getData = async () => {
+    //     try {
+    //         const response = await axios.get(`https://swapi.dev/api/films`);
+    //         setData(response.data);
+    //         setError(null);
+    //     } catch (err) {
+    //         setError(err.message);
+    //         setData(null);
+    //     } finally {
+    //         setLoading(false);
+    //     }
+    // };
+
+    // getData();
+
+      }, [])
+  return (
+    <>
+      <div style={{background:"black"}}>
+        <h1>.</h1>
+        {loading && <div>Data is loading. Please wait...</div>}
+        {error && <div>{`There is a problem fetching your data - ${error}`}</div>}
+        <ul>
+            {data && data.map((item) => {
+                      return (
+                          <li className='card' key={item.episode_id}>
+                              <div>
+                                <div className='text'>
+                                  <div className='title'><h2>{item.title}</h2></div>
+                                  <div className='date'><p>{item.release_date}</p></div>
+                                  <div className='redborder'><p>{item.opening_crawl}</p></div>
+                                  <div ><MoreInfoBtn text={'More Info'}/></div>
+                                </div> 
+                              </div>
+                          </li>
+                      )
+                  })}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Films
---
+++ star-wars/src/Components/MoreInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MoreInfo = (props) => {
+  return (
+    <div>
+        <a style={{color:"yellow", marginTop:"20px"}} href='Link'>{props.text}</a>
+    </div>
+  )
+}
+
+export default MoreInfo
